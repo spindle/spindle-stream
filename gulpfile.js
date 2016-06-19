3f8bd5befc6323c94cdacfd438fe9d53e9314fc1
Hiraku NAKANO <hiraku@tojiru.net>
2016-06-19 11:16:44 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -3,62 +3,65 @@
  *
  * 1. install node.js & npm
  * 2. $ npm install
- * 3. $ gulp server
- * 4. open http://localhost:9000/ (livereload enabled)
- * 5. coding on src/*.php and tests/*.php
- *
- * enjoy!
- *
- * @license https://creativecommons.org/publicdomain/zero/1.0/ CC0-1.0 (No Rights Reserved.)
- * @link https://github.com/spindle/spindle-lib-template
+ * 3. $ npm start
  */
 var gulp = require('gulp');
 var exec = require('child_process').exec;
-var connect = require('gulp-connect');
+var bs = require('browser-sync').create();
 
 gulp.task('default', ['test', 'inspect']);
 
-gulp.task('help', function(){
-    console.log('gulp test\t... kick vendor/bin/phpunit command');
-    console.log('gulp inspect\t... kick vendor/bin/apigen and vendor/bin/pdepend');
-    console.log('gulp server\t... start static web server on http://localhost:9000/');
-    console.log('\tcoverage report... http://localhost:9000/coverage/');
-    console.log('\tApiGen document... http://localhost:9000/api/');
-});
+function test(done) {
+    var p = exec('composer fasttest');
+    p.stdout.pipe(process.stdout);
+    p.stderr.pipe(process.stderr);
+    p.on('exit', done);
+}
 
-gulp.task('test', function(done){
-    exec('vendor/bin/phpunit', function(err, stdout, stderr){
-        console.log(stdout);
-        console.error(stderr);
-        done();
+function inspect(done) {
+    var r = 3;
+    function wait() {
+        --r || done();
+    }
+    exec('composer doc', wait);
+    exec('composer metrics', wait);
+    var lint = exec('composer lint'); 
+    lint.stdout.pipe(process.stdout);
+    lint.stderr.pipe(process.stderr);
+    lint.on('exit', wait);
+}
+
+function lint(target, done) {
+    var p = exec('composer lint -- ' + target);
+    p.stdout.pipe(process.stdout);
+    p.stderr.pipe(process.stderr);
+    p.on('exit', done);
+}
+
+gulp.task('test', test);
+gulp.task('inspect', inspect);
+
+gulp.task('start', function(){
+    bs.init({
+        server: {
+            baseDir: "artifacts/"
+        },
+        ghostMode: false
     });
-});
 
-gulp.task('inspect', function(done){
-    var i = 0, count = function(){ if (++i > 1) done() };
-    exec([
-        'vendor/bin/pdepend',
-        '--jdepend-chart=builds/pdepend.svg',
-        '--overview-pyramid=builds/pyramid.svg',
-        '--summary-xml=builds/summary.xml',
-        'src/'].join(' '), count);
-    exec('vendor/bin/apigen.php', count);
-});
-
-gulp.task('connect', ['default'], function(){
-    connect.server({
-        root: [__dirname + '/builds/'],
-        port: 9000,
-        livereload: true
+    gulp.watch(['src/**/*.php', 'tests/**/*Test.php'], {}, function(ev){
+        switch (ev.type) {
+            case "added":
+            case "changed":
+                lint(ev.path, function(){
+                    test(bs.reload);
+                });
+                break;
+            default:
+                inspect(function(){
+                    test(bs.reload);
+                });
+        }
     });
-});
-
-gulp.task('reload', ['test'], function(){
-    return gulp.src('builds/coverage/**')
-        .pipe(connect.reload());
-});
-
-gulp.task('server', ['connect'], function(){
-    gulp.watch(['src/**', 'tests/**'], ['reload']);
 });
 
